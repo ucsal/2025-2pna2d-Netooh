fcda57bd021a22d725af3382698762aaa9ae57bf
Netooh <celsoneto95@hotmail.com>
2025-09-02 00:26:41 -0300

    refactor: introduz MailSender, adapta SmtpClient, cria NoopMailSender e atualiza EmailNotifier

---
+++ src/main/java/br/com/mariojp/solid/dip/NoopMailSender.java
@@ -0,0 +1,8 @@
+package br.com.mariojp.solid.dip;
+
+public class NoopMailSender implements MailSender {
+    @Override
+    public void send(String to, String subject, String body) {
+        // não faz nada
+    }
+}
